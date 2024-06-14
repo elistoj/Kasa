7bf03e9d7209a393d1f24e27d695ae936b5457e4
elistoj <atanastojaneli@gmail.com>
2024-06-15 00:09:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// RouterApp.js
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
@@ -7,33 +5,24 @@ import About from './pages/A-propos/A-propos';
 import NotFound from './pages/NotFound/NotFound';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import logoHeader from './assets/images/logo.png';
-import logoFooter from './assets/images/LOGO2.png';
 import './App.css';
+import FicheLogement from './pages/Fiche-Logement/Fiche-Logement';
 
 const RouterApp = () => {
-  const headerData = {
-    logoPath: logoHeader,
-    links: [
-      { path: '/', label: 'Accueil' },
-      { path: '/about', label: 'À propos' }, 
-    ],
-  };
-
   const footerData = {
-    logoPath: logoFooter,
     text: '© 2020 Kasa. All rights reserved',
   };
 
   return (
     <Router>
-      <Header logo={headerData.logoPath} links={headerData.links} />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} /> 
+        <Route path="/logement/:id" element={<FicheLogement />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer logo={footerData.logoPath} text={footerData.text} />
+      <Footer text={footerData.text} />
     </Router>
   );
 };
