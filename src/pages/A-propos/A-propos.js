22edb9ace035ba03e6d1b35fbb4d6f7ac069a276
elistoj <atanastojaneli@gmail.com>
2024-06-15 00:05:48 +0200

    Update A-propos.js

--- src/pages/A-propos/A-propos.js
+++ src/pages/A-propos/A-propos.js
@@ -2,39 +2,29 @@ import React from 'react';
 import Banner from '../../components/Banner/Banner';
 import Collapse from '../../components/Collapse/Collapse';
 import bannerImage from '../../assets/images/banner.png';
-import '../../components/Collapse/Collapse.css';
-import './A-propos.css'
-
+import textData from '../../assets/text-collapse.json'; 
+import './A-propos.css';
 
 const About = () => {
   return (
     <main>
-      <Banner  image={bannerImage} />
-      <div className="apropos-content">
+      <Banner image={bannerImage} />
+      <section className="apropos-content">
         <div className="collapses-container">
-          <Collapse title="Fiabilité">
-            <p>Les annonces postees sur Kasa garantissent une fiabilite totale. Les photos sont conformes aux logements, et
-              toutes les informations sont regulierement verifiees par nos equipes.
-            </p>
+          <Collapse title="Fiabilité" className="reliability-collapse">
+            <p>{textData.reliability}</p>
           </Collapse>
-          <Collapse title="Respect">
-            <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
-              perturbation du voisinage entralnera une exclusion de notre plateforme.
-            </p>
+          <Collapse title="Respect" className="respect-collapse">
+            <p>{textData.respect}</p>
           </Collapse>
-          <Collapse title="Service">
-            <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
-            perturbation du voisinage entralnera une exclusion de notre plateforme.</p>
+          <Collapse title="Service" className="service-collapse">
+            <p>{textData.service}</p>
           </Collapse>
-          <Collapse title="Sécurité">
-            <p>La securite est la priorite de Kasa. Aussi bien pour nos hates que pour les voyageurs, chaque logement
-              correspond aux criteres de securite etablis par nos services. En laissant une note aussi bien a l'hate qu'au
-              locataire, cela permet a nos equipes de verifier que les standards sont bien respectes. Nous organisons
-              egalement des ateliers sur la securite domestique pour nos hates.
-            </p>
+          <Collapse title="Sécurité" className="security-collapse">
+            <p>{textData.security}</p>
           </Collapse>
         </div>
-      </div>
+      </section>
     </main>
   );
 };
