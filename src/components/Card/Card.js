f6f82abe2b7951e810d506a12264a8200ee679dd
elistoj <atanastojaneli@gmail.com>
2024-06-15 00:09:12 +0200

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Card.css';
 
-const Card = ({ title, image }) => {
+const Card = ({ id, title, image }) => {
   return (
-    <div className="card">
-      <img src={image} alt={title} className="card-image" />
-      <div className="card-title">{title}</div>
-    </div>
+    <Link to={`/logement/${id}`} className="card-link">
+      <div className="card">
+        <img src={image} alt={title} className="card-image" />
+        <div className="card-title">{title}</div>
+      </div>
+    </Link>
   );
 };
 
