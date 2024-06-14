016ca3b337b0f36229ee66a3cdcd7fbef166b008
elistoj <atanastojaneli@gmail.com>
2024-06-15 00:05:59 +0200

    Create UnderlinedNavLink.js

---
+++ src/components/UnderlinedNavLink/UnderlinedNavLink.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import './UnderlinedNavLink.css';
+
+const UnderlinedNavLink = ({ to, label }) => {
+  const location = useLocation();
+
+  return (
+    <NavLink
+      to={to}
+      className={`home-nav-link ${location.pathname === to ? "underline-on-active" : ""}`}
+    >
+      {label}
+    </NavLink>
+  );
+};
+
+export default UnderlinedNavLink;
