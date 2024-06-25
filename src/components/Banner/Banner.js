34140702d54baf7fd91401f83275d96a4c341176
elistoj <atanastojaneli@gmail.com>
2024-06-25 14:23:39 +0200

    details

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Banner.scss';
 
-const Banner = ({ title, image }) => (
-  <div className="banner"> {/* Conteneur principal de la bannière */}
-    <img src={image} alt="Banner" className="banner-img" /> {/* Image de la bannière avec un texte alternatif */}
-    <div className="banner-title">{title}</div> {/* Titre de la bannière */}
-  </div>
-);
+const Banner = ({ title = '', image }) => {
+  useEffect(() => {
+    const updateBannerText = () => {
+      const bannerTitleElement = document.querySelector('.banner-title');
+      if (bannerTitleElement && title) {
+        if (window.innerWidth <= 480) {
+          const text = title.replace(/,/g, ',\n');
+          bannerTitleElement.innerText = text;
+        } else {
+          bannerTitleElement.innerText = title.replace(/\n/g, '');
+        }
+      }
+    };
 
-export default Banner; 
+     updateBannerText();
 
+     window.addEventListener('resize', updateBannerText);
+
+     return () => {
+      window.removeEventListener('resize', updateBannerText);
+    };
+  }, [title]);
+
+  return (
+    <div className="banner">
+      <img src={image} alt="Banner" className="banner-img" />
+      <div className="banner-title">{title}</div>
+    </div>
+  );
+};
+
+export default Banner;
